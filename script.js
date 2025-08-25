81420beba1878bb704666fd2bf85c12bd5ca466c
GabrielaPSantos <gabrielaperessantos1@gmail.com>
2025-08-25 20:15:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Efeito Typed.js
+const typed = new Typed("#typed", {
+  strings: ["Desenvolvedora Web,", "Front-end"],
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 1500,
+  loop: true
+});
+
+
+//Envio do formulario
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    const comentario = document.getElementById('comentario').value
+
+    const destinatario = "sgabi4813@gmail.com"
+    const assunto = 'Sobre seu Potfólio'
+    const corpo = `Olá me chamo ${nome}, ${comentario}`
+
+     const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${encodeURIComponent(assunto)}&body=${encodeURIComponent(corpo)}`;
+
+     window.open(gmailLink, '_blank');
+
+})
